5924db6a1b5fa27863386c0def9f08ca07dd3feb
Adna B <73432374+adna98@users.noreply.github.com>
2022-06-25 22:57:09 +0200

    Initial Commit

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def salary(wh_number):
 total_salary = salary(wh_number)
 print(total_salary)
 
-vacation = input("Pleas enter if you have vacation days in this month\n")
+vacation = input("Pleas enter the number of your vacation days in this month\n")
 vacation_number = int(vacation)
 field_fee = input("Pleas enter your field days for this month\n")
 field_fee_number = int(field_fee)
@@ -32,23 +32,31 @@ def total(field_fee_number):
 total_number = total(field_fee_number)
 print(total_number)
 
-user_input = input("Pleas enter your total salary to know your finance stability\n")
-user_input_number = float(user_input)
+Q_input = input("Do you want to know your finance stability (Answer this question with yes or no) \n")
+Q1 = "yes"
+Q2 = "no"
 
 
-def answare(user_input_number):
-    total_number_br = float(user_input_number)
-    print(user_input_number > 1667)
-    if user_input_number > 1667:   # this is the average gross salary in FBiH for 04/2022
-        return "Your salary is bigger then average gross salary in FBiH this days\n" \
-               "Congratulations, and wish you much more success in your future career"
-    elif user_input_number <= 1000:
-        return "I am really sorry telling this to you, but your salary is much below average gross salary in FBiH\n " \
-               "I belive that you can do it better!\n" \
-               "Good luck with finding new job!"
-    else:
-        return "Your salary is below average gross salary in FBiH"
+if Q_input == Q1:
+    user_input = input("Pleas enter your total salary to know your finance stability\n")
+    user_input_number = float(user_input)
 
 
-final_answare = answare(user_input_number)
-print(final_answare)
+    def answare(user_input_number):
+        if user_input_number > 1667:  # this is the average gross salary in FBiH for 04/2022
+            return "Your salary is bigger then average gross salary in FBiH this days\n" \
+                   "Congratulations, and wish you much more success in your future career"
+        elif user_input_number <= 1000:
+            return "I am really sorry, but your salary is much below average gross salary in FBiH\n " \
+                   "I belive that you can do it better!\n" \
+                   "Good luck with finding new job!"
+        else:
+            return "Your salary is below average gross salary in FBiH"
+
+
+    final_answare = answare(user_input_number)
+    print(final_answare)
+elif Q_input == Q2:
+    print("Thank you for using this program, I hope you found it useful")
+else:
+    print("Your answer is not complete please enter yes or no")
